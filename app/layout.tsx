5932f20db5160edcb0b8ea9efd38d2540c04104a
JonMoore9000 <jon@veriheal.com>
2025-04-30 14:13:36 -0600

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { comfortaa, flower, ibmMono } from './fonts';
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,6 +27,7 @@ export default function RootLayout({
         
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
